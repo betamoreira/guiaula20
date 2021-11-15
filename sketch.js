e6915c0729171f63b8bce84e9523bef2f70eec8a
Roberta Moreira <berta.santos.moreira@gmail.com>
2021-11-15 19:47:01 -0400

    aula20

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  retFixo = createSprite(600,200,50,80);
+  retFixo.shapeColor = "green";
+  retFixo.debug = true;
+  retMovendo = createSprite(400,800,80,30);
+  retMovendo.shapeColor = "green";
+  retMovendo.debug = true;
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255); 
+  
+  retMovendo.x = World.mouseX;
+  retMovendo.y = World.mouseY;
+
+  if(retMovendo.x - retFixo.x < retFixo.width/2 + retMovendo.width/2){
+    retFixo.shapeColor = "red";
+    retMovendo.shapeColor = "red";
+  }
+   else {
+    retFixo.shapeColor = "green";
+    retMovendo.shapeColor = "green";
+   }
+
+  console.log(retFixo.x - retMovendo.x);
+
   drawSprites();
 }
